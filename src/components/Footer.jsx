52a46d4d8534de45eadc3d8806834139171f73b0
Mergim Januzi <mergim.januzi@gmail.com>
2025-05-04 14:26:18 +0200

    Footer jsx started with descriptions on path

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import {Link, useLocation} from "react-router-dom";
+import {useAuth} from "../hooks/useAuth";
+
+/**Footer component to the bottom of the screen for mobile
+ * 
+ * Change options from Login to My profile when logged on
+ */
+
