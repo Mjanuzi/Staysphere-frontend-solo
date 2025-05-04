7a1b26728488a1b1ae85eb93716ac0e962ff2df7
Mergim Januzi <mergim.januzi@gmail.com>
2025-05-04 14:33:51 +0200

    made a new folder for css in components to separate css to jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,11 @@ import {useAuth} from "../hooks/useAuth";
 
 /**Footer component to the bottom of the screen for mobile
  * 
- * Change options from Login to My profile when logged on
+ * Change options from Login to My profile when logged on based on authentication status.
  */
+
+const Footer = () => {
+    const location = useLocation();
+    const {currentUser} = useAuth();
+}
 
