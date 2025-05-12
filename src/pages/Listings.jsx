84f3e3d2a350a0f07c3a328d75e0dd4f765998e7
Mergim Januzi <mergim.januzi@gmail.com>
2025-05-12 18:29:51 +0200

    forgot to import listings.css file to listings.jsx

--- src/pages/Listings.jsx
+++ src/pages/Listings.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useListings } from "../hooks/useListings";
+import "./Listings.css";
 
 /**Listing page
  * Display all listings, fetching data from backend
