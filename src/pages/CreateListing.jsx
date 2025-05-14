7e5b1f806a14941e4bd5f30ff64b3d39c9640e05
Mergim Januzi <mergim.januzi@gmail.com>
2025-05-14 19:45:00 +0200

    setting up formdata from backend to match setters in client

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,15 +1,29 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {useAuth} from "../hooks/useAuth"
-
-
+import { useAuth } from "../hooks/useAuth";
 
 /* Create listing page
  *that lets user create a listing for their property */
 
 const CreateListing = () => {
-    const navigate = useNavigate();
-    const {userId} = useAuth();
-    const[loading, setLoading] = useState(false);
-    const [errorMessage, setErrorMessage] =useState("");
-}
+  const navigate = useNavigate();
+  const { userId } = useAuth();
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const [formData, setFormData] = useState({
+    hostId: userId,
+    listingTitle: "",
+    listingDescription: "",
+    listingPricePerNight: "",
+    guestLimit: 1,
+    listingImages: [],
+  });
+
+
+
+
+
+
+  
+};
