bc7f93dca25989abce85e5cea5895c8e6c3d0d5f
Mergim Januzi <mergim.januzi@gmail.com>
2025-05-14 19:26:32 +0200

    creating variables/states for createlisting

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -6,3 +6,10 @@ import {useAuth} from "../hooks/useAuth"
 
 /* Create listing page
  *that lets user create a listing for their property */
+
+const CreateListing = () => {
+    const navigate = useNavigate();
+    const {userId} = useAuth();
+    const[loading, setLoading] = useState(false);
+    const [errorMessage, setErrorMessage] =useState("");
+}
